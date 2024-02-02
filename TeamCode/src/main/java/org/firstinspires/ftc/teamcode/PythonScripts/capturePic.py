9c0a9f01eb58bcef99ae50cd918f877ae2fbbc49
VladTomici14 <vladtomici@gmail.com>
2024-02-02 15:29:50 +0200

    Added the posibility to take more pictures from one run of the script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PythonScripts/capturePic.py
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PythonScripts/capturePic.py
@@ -1,19 +1,27 @@
 import cv2
 import argparse
 
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 864)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+# TODO: adapt for multiple shots
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-H", "--height", required=False, type=int, default=0, help="target height for reshape")
 ap.add_argument("-W", "--width", required=False, type=int, default=0, help="target width for reshape")
 args = vars(ap.parse_args())
 
+i = 1
 while True:
     ret, frame = camera.read()
     if ret:
         cv2.imshow("da", frame)
+
+        if cv2.waitKey(1) == ord("t"):
+            cv2.imwrite(f"image{i}.png", frame)
+            i += 1
+
         if cv2.waitKey(1) == ord("q"):
             if args["height"] != 0 and args["width"] != 0:
                 frame = cv2.resize(frame, (args["width"], args["height"]), interpolation=cv2.INTER_AREA)
