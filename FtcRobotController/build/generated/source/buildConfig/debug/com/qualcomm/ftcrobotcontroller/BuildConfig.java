ece9629f32dace18f891e92f18e25d0e3b4eab89
Vlad Tomici <vladtomici@gmail.com>
2024-03-07 12:28:25 +0200

    Added build files

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-02-24T09:52:57.549+0200";
+  public static final String APP_BUILD_TIME = "2024-02-25T09:08:45.807+0200";
 }
